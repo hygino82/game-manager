2ad44da4ddf70d1554d11082a2087ba2f74c9204
Adroaldo <hygino@msn.com>
2024-07-21 21:53:19 -0300

    Iniciando a criação do backend

---
+++ backend/src/test/java/br/dev/hygino/game_manager/GameManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.hygino.game_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
